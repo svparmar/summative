ff7194bbb24609e0f3353ba448801211e483f09a
svparmar <soniap@sonias-mbp.wlan.dur.ac.uk>
2019-12-10 17:22:57 +0000

    addingfiles

---
+++ sum1/csvSUM.js
@@ -0,0 +1,13 @@
+//d3.csv('url of github raw').then(function(data))
+
+//d3.csv("https://raw.githubusercontent.com/svparmar/summative/master/sum1/cut%20version%20of%20data%20copy.csv?token=AKBP7R3QKCOUWM4XD64GIOC5566HW").then(function(data){});
+
+d3.csv("https://raw.githubusercontent.com/svparmar/summative/master/sum1/cut%20version%20of%20data%20copy.csv?token=AKBP7R435RLSEQEB4PCXRFK557JXG",function(data) {
+	console.log(data[0]);
+});
+
+//d3.csv("https://raw.githubusercontent.com/svparmar/summative/master/sum1/cut%20version%20of%20data%20copy.csv?token=AKBP7R3QKCOUWM4XD64GIOC5566HW").then(function(data) {
+ // console.log(data[0]);
+
+//});
+
